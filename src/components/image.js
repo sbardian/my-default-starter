9a541d85cfd0e30d3a3dcf296d430036cc020ce9
Brian Andrews <sbardian@gmail.com>
2019-11-14 11:05:51 -0700

    update for testing PR 17006 against gatsby current

--- src/components/image.js
+++ src/components/image.js
@@ -2,25 +2,44 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      maxWH: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
-          fluid(maxWidth: 800, maxHeight: 800) {
-            maxWidth
-            maxHeight
+          fluid(maxWidth: 200, maxHeight: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      maxW: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      maxH: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxHeight: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      cropWidth: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200, maxHeight: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      cropHeight: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600, maxHeight: 200) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -28,7 +47,36 @@ const Image = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return (
+    <>
+      <span>
+        maxWidth: 200, maxHeight: 200. Restrict width/height fluid smaller
+      </span>
+      <Img fluid={data.maxWH.childImageSharp.fluid} />
+
+      <hr />
+
+      <span>maxWidth: 500, fluid smaller</span>
+      <Img fluid={data.maxW.childImageSharp.fluid} />
+
+      <hr />
+
+      <span>maxHeight: 500, fluid smaller</span>
+      <Img fluid={data.maxH.childImageSharp.fluid} />
+
+      <hr />
+
+      <span>maxWidth: 200, maxHeight: 600. Crop Width</span>
+      <Img fluid={data.cropWidth.childImageSharp.fluid} />
+
+      <hr />
+
+      <span>maxHeight: 200, maxWidth: 600. Crop Height</span>
+      <Img fluid={data.cropHeight.childImageSharp.fluid} />
+
+      <hr />
+    </>
+  )
 }
 
 export default Image
